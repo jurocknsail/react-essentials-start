850d0d3a48fab023c75b48022586bfe0cf3d9270
Julien Berger <julien.berger1421@gmail.com>
2024-08-12 13:58:35 +0000

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcepts.jsx";
 import Examples from "./components/Examples.jsx";
